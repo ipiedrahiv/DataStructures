c9ab284e2020c878d9dca695dc1fa7df2d38366a
Isabel Piedrahita <46933373+ipiedrahiv@users.noreply.github.com>
2019-09-17 16:03:20 -0500

    Create Activity.java

---
+++ MidTerm/Activity.java
@@ -0,0 +1,15 @@
+public class Activity {
+
+    int ID;
+    int start;
+    int end;
+    String description;
+
+    public Activity(int ID, int start, int end, String description) {
+        this.ID = ID;
+        this.start = start;
+        this.end = end;
+        this.description = description;
+    }
+
+}
